bc62d3ba9a3fc32cedfe734c5cc3c370b9b68d32
Denis Li <denisli268@gmail.com>
2018-07-16 21:27:10 -0700

    Implemented feature so that typing a query into the popup input will populate a list of candidates from list of open tabs.
    Still need to implement interaction with the list so that users can use
    the extension to efficiently switch tabs.
    
    Furthermore, the query-matching algorithm is very rudimentary right now.
    It simply does a subtring check against the url and titles of the tabs.

--- popup.js
+++ popup.js
@@ -15,5 +15,39 @@ browser.runtime.sendMessage({}, function() {
 // Listen for response to handshake from background script
 browser.runtime.onMessage.addListener(function() {
   console.log("Receives background script's response to handshake");
-  document.getElementById('tt-query-input').focus();
+  // Focus on input and list initial candidates
+  const input = document.getElementById('tt-query-input')
+  input.focus();
+  listCandidateTabs(input.value);
+
+  // Add a listener to the input so that candidates are repopulated
+  input.addEventListener('input', function(evt) {
+    let queryStr = evt.target.value;
+    listCandidateTabs(queryStr);
+  });
 });
+
+const listCandidateTabs = function(queryStr) {
+  browser.tabs.query({currentWindow: true})
+    .then((tabs) => {
+      const matchingTabs = getMatchingTabs(tabs, queryStr);
+      populateCandidateTabsContainer(matchingTabs);
+    });
+};
+
+const getMatchingTabs = function(tabs, queryStr) {
+  const matches = tabs.filter((tab) => {
+    return (tab.url.includes(queryStr) || tab.title.includes(queryStr));
+  });
+  return matches;
+};
+
+const populateCandidateTabsContainer = function(tabs) {
+  const container = document.getElementById('tt-candidate-tabs');
+  container.innerHTML = "";
+  for (let tab of tabs) {
+    const p = document.createElement('p');
+    p.innerHTML = tab.url;
+    container.appendChild(p);
+  }
+};
